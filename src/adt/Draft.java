871d524af638cd62b0decb5825532f59f62c7eee
Ngoc Hien Le <ngochien.le@haw-hamburg.de>
2014-10-01 00:52:43 +0200

    first commit

---
+++ src/adt/Draft.java
@@ -0,0 +1,29 @@
+/*
+ * Hamburg University of Applied Sciences
+ *
+ * Programming assignments
+ *
+ * ngochien.le@haw-hamburg.de
+ */
+package adt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Le
+ */
+public class Draft {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 1; i++) {
+			System.out.println(i);
+		}
+		List<String> l = null;
+		ArrayList<String> ar;
+		// System.out.println(l.equals(null));
+		SinglyLinkedList<String> sl = new SinglyLinkedList<>();
+		System.out.println(sl.getClass().getName().equals("adt.SinglyLinkedList"));
+		System.out.println(!(sl instanceof SinglyLinkedList<?>));
+	}
+}
