0c6856a57e924cc84cc317eee47ebffefd54e6e9
Ngoc Hien Le <ngochien.le@haw-hamburg.de>
2014-10-23 01:37:17 +0200

    algebraicExpression und test case fertig

---
+++ src/adt/Algebra.java
@@ -0,0 +1,66 @@
+/*
+ * Hamburg University of Applied Sciences
+ *
+ * Programming assignments
+ *
+ * ngochien.le@haw-hamburg.de
+ */
+package adt;
+
+/**
+ *
+ *
+ * @author Le
+ * @author Nguyen
+ */
+public class Algebra {
+
+	public static int algebraicExpression(String expression) {
+		expression = expression.replaceAll("\\s", "");
+		char[] a = expression.toCharArray();
+		Stack<String> stack = new ArrayStack<>(a.length);
+		int tmpResult = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] >= '0' && a[i] <= '9') {
+				stack.push(0 + "");
+			}
+			while (a[i] >= '0' && a[i] <= '9') {
+				tmpResult = 10 * Integer.parseInt(stack.top()) + a[i] - '0';
+				stack.push(tmpResult + "");
+				i++;
+			}
+			while (i < a.length && a[i] == ')') {
+				int operand2 = Integer.parseInt(stack.top());
+				String operator = stack.top();
+				int operand1 = Integer.parseInt(stack.top());
+				stack.pop();
+
+				switch (operator) {
+				case "+":
+					tmpResult = operand1 + operand2;
+					stack.push(tmpResult + "");
+					break;
+				case "-":
+					tmpResult = operand1 - operand2;
+					stack.push(tmpResult + "");
+					break;
+				case "*":
+					tmpResult = operand1 * operand2;
+					stack.push(tmpResult + "");
+					break;
+				case "/":
+					tmpResult = operand1 / operand2;
+					stack.push(tmpResult + "");
+					break;
+				}
+				i++;
+			}
+			if (i < a.length) {
+				stack.push(a[i] + "");
+			}
+		}
+
+		return Integer.parseInt(stack.top());
+	}
+}
---
+++ src/adt/AlgebraTest.java
@@ -0,0 +1,34 @@
+/*
+ * Hamburg University of Applied Sciences
+ *
+ * Programming assignments
+ *
+ * ngochien.le@haw-hamburg.de
+ */
+package adt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ *
+ *
+ * @author Le
+ */
+public class AlgebraTest {
+
+	/**
+	 * Test method for {@link adt.Algebra#algebraicExpression(java.lang.String)}.
+	 */
+	@Test
+	public void testAlgebraicExpression() {
+		String expression = "((2 * (13 - ((9 + 6) * 15))) - (8 / (4 - 2)))";
+		assertEquals(-428, Algebra.algebraicExpression(expression));
+		String s1 = "((6 * (4 + 2)) + (5 - 1))";
+		assertEquals(40, Algebra.algebraicExpression(s1));
+		String s2 = "(((1 + 2) * 3) - (7 * 8))";
+		assertEquals(-47, Algebra.algebraicExpression(s2));
+	}
+
+}
